b5b2d8a255a0c45d9adb2895c4de2768c1e52f56
GallabiGaminG <bersan_guzel3306@hotmail.com>
2024-06-02 18:43:47 +0300

    Çalışma ve testler
    Eski kod denemeleri

---
+++ Kod/C/Projects/DevC_calisma/egitim.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+// #include<conio.h> //Windows'un eski sürümlerinde derlenen program anýnda kapanýyorsa...
+int main (){
+
+	printf("Hat\tGG\nYeni satir");
+	//getch(); // conio.h kütüphanesinden geliyor.
+	
+	
+	return 0;}
